b5664b8696d61dac03d606e4f93d340c6fa0a6c3
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-02-18 19:48:57 -0500

    Prefer <ul> over <div> for Suggestions markup

--- src/ui/Suggestions.jsx
+++ src/ui/Suggestions.jsx
@@ -3,13 +3,13 @@ const React = require('react');
 class Suggestions extends React.Component {
   render() {
     return (
-      <div className="suggestions">
+      <ul className="suggestions">
         {
           (this.props.suggestions || []).map((el, i) => {
             return <li key={ `suggestion-${i}`}>{ el.term }</li>;
           })
         }
-      </div>
+      </ul>
     );
   }
 }
