2e06b4142d7d10c291959189bda31bdb1fe59127
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-02-22 08:08:32 -0500

    Save compiled JS to public/js/ dir.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.jsx',
   output: {
-    filename: './main.js'
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public/js'),
   },
   module: {
     rules: [
